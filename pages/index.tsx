15f89c5c4ce4ce8f894cb3aabc043641e34cc159
niemisami <sami@nieminen.fi>
2023-01-19 18:20:45 +0200

    Add nice colors to frontpage

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,37 @@ export default function Page() {
       <Head>
         <title>Wappulehti Pönkeli</title>
       </Head>
+      <svg className='w-full h-full absolute'>
+        <defs>
+          <linearGradient id='logo-gradient' x1='50%' y1='0%' x2='50%' y2='100%'>
+            <stop offset='0%' stop-color='#B2A4FF'>
+              <animate
+                attributeName='stop-color'
+                values='#B2A4FF;#FFB4B4;#B2A4FF;#f2b880;#FFDEB4;#DBF9F4;#B2A4FF'
+                dur='10s'
+                repeatCount='indefinite'
+              ></animate>
+            </stop>
+            <stop offset='100%' stop-color='#A85751'>
+              <animate
+                attributeName='stop-color'
+                values='#A85751;#A85751;#B2A4FF;#DBF9F4;#f2b880;#FFB4B4;#FFDEB4;#A85751'
+                dur='10s'
+                repeatCount='indefinite'
+              ></animate>
+            </stop>
+          </linearGradient>
+        </defs>
+        <g>
+          <rect
+            x='0'
+            y='0'
+            width='100%'
+            height='100%' 
+            fill="url('#logo-gradient')"
+          />
+        </g>
+      </svg>
       <div className='hero-content text-center'>
         <div className='max-w-md'>
           <h2 className='text-xl font-bold'>Turun Yliopiston<br />teekkareiden Wappulehti</h2>
