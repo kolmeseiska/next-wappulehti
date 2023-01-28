62cff1d8ae0762a7b60145fb922eecf39605ed69
niemisami <sami@nieminen.fi>
2023-01-28 11:25:27 +0200

    Create 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+
+export default function FourOhFour() {
+  return (
+    <div className='hero min-h-screen bg-base-200'>
+      <div className='hero-content text-center'>
+        <div className='max-w-md'>
+          <h1 className='text-9xl font-extrabold text-zinc-400'>404</h1>
+          <p className='py-7 text-xl text-zinc-400'>
+            Sorry! Täällä ei ole mitään
+            {' '}
+            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' className='w-6 h-6 inline-block align-baseline'>
+              <path d='M10.5 1.875a1.125 1.125 0 012.25 0v8.219c.517.162 1.02.382 1.5.659V3.375a1.125 1.125 0 012.25 0v10.937a4.505 4.505 0 00-3.25 2.373 8.963 8.963 0 014-.935A.75.75 0 0018 15v-2.266a3.368 3.368 0 01.988-2.37 1.125 1.125 0 011.591 1.59 1.118 1.118 0 00-.329.79v3.006h-.005a6 6 0 01-1.752 4.007l-1.736 1.736a6 6 0 01-4.242 1.757H10.5a7.5 7.5 0 01-7.5-7.5V6.375a1.125 1.125 0 012.25 0v5.519c.46-.452.965-.832 1.5-1.141V3.375a1.125 1.125 0 012.25 0v6.526c.495-.1.997-.151 1.5-.151V1.875z' />
+            </svg>
+
+          </p>
+          <div>
+            <Link href='/' className='link-primary font-bold uppercase'>
+              Palaa kotiin 
+              {' '}
+              <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' className='w-5 h-5 inline-block align-text-bottom'>
+                <path d='M19.006 3.705a.75.75 0 00-.512-1.41L6 6.838V3a.75.75 0 00-.75-.75h-1.5A.75.75 0 003 3v4.93l-1.006.365a.75.75 0 00.512 1.41l16.5-6z' />
+                <path fillRule='evenodd' d='M3.019 11.115L18 5.667V9.09l4.006 1.456a.75.75 0 11-.512 1.41l-.494-.18v8.475h.75a.75.75 0 010 1.5H2.25a.75.75 0 010-1.5H3v-9.129l.019-.006zM18 20.25v-9.565l1.5.545v9.02H18zm-9-6a.75.75 0 00-.75.75v4.5c0 .414.336.75.75.75h3a.75.75 0 00.75-.75V15a.75.75 0 00-.75-.75H9z' clipRule='evenodd' />
+              </svg>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  )
+}
