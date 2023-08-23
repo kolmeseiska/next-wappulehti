7f2b4ecdbec62443b153d89d1c838463f9bf5d95
niemisami <sami@nieminen.fi>
2023-08-23 22:44:28 +0300

    Add 2023 pönkeli history

--- pages/historia.tsx
+++ pages/historia.tsx
@@ -1,5 +1,43 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Image from 'next/image'
+
+const editions = [
+  {
+    year: 2023,
+    title: 'FantasiaPönkeli',
+    body: (
+      <>
+        <p>
+          Vuonna 2023 otettiin askel perinteisempään ja Pönkeli manifestoitui lehden muotoon. <b>&quot;FantasiaPönkeli&quot;</b> on fantasia/DnD/roolipeli-teemainen tuotos täynnä upeaa perinteisin sekä virtuaalisin keinoin loihdittua grafiikkaa, tarinoita sekä vitsejä.
+        </p>
+        <p>
+          Pelkän luettavan lisäksi käyttäjät saivat itselleen viisi elementtinoppaa, joita pystyi hyödyntämään lehden sivuilta löytyvissä peleissä sekä vaihtelemaan toisten pönkelifantasian ystävien kanssa.
+        </p>
+      </>
+    ),
+    imageSource: '/ponkelipromo23-5.webp'
+  },
+  {
+    year: 2022,
+    title: 'Pönkeliseikkailu',
+    body: (
+      <p>
+        Vuoden 2022 wappulehdessä <b>&quot;Pönkeliseikkailu&quot;</b> lukijat pääsivät etsimään kadonnutta tutkintoa upean grafiikan saattelemana.
+      </p>
+    ),
+    imageSource: '/ponkelijulkkari-1.jpg'
+  },
+  {
+    year: 2021,
+    title: 'Pönkelipakka',
+    body: (
+      <p>
+        Ensimmäinen Pönkeli näki päivänvalon wappuna 2021 nimellä &quot;Pönkelipakka&quot;. Pelikorttipakka, jota monet turkulaiset sekä ulkopaikkakuntalaiset opiskelijat kantavat yhä haalareissaan.
+      </p>
+    ),
+    imageSource: '/wappulehti-2021.jpg'
+  }
+]
 
 const historia = () => {
   return (
@@ -12,32 +50,21 @@ const historia = () => {
         <p>
           Teekkareiden Wappulehtiä on väännetty Turun yliopistossa jo vuodesta 2001 ja tuolloin teoksen nimi oli Pilde&Napander. Vuonna 2020 projekti aloitettiin alusta ja wappulehti sai uuden uljaan ja soiluvan nimen <b>Pönkeli.</b>
         </p>
-        <h3 className='text-2xl font-extrabold'>
-          2022
-        </h3>
-        <p>
-          Vuoden 2022 wappulehdessä <b>&quot;Pönkeliseikkailu&quot;</b> lukijat pääsivät etsimään kadonnutta tutkintoa upean grafiikan saattelemana.
-        </p>
-        <Image
-          src='/ponkelijulkkari-1.jpg'
-          width={600}
-          height={500}
-          alt='wappulehti-2022'
-          className='mx-auto'
-        />
-        <h3 className='text-2xl font-extrabold'>
-          2021
-        </h3>
-        <p>
-          Ensimmäinen Pönkeli näki päivänvalon wappuna 2021 nimellä &quot;Pönkelipakka&quot;. Pelikorttipakka, jota monet turkulaiset sekä ulkopaikkakuntalaiset opiskelijat kantavat yhä haalareissaan.
-        </p>
-        <Image
-          src='/wappulehti-2021.jpg'
-          width={600}
-          height={500}
-          alt='wappulehti-2021'
-          className='mx-auto'
-        />
+        {editions.map(edition => (
+          <Fragment key={edition.year}>
+            <h3 className='text-2xl font-extrabold'>
+              {edition.year} - {edition.title}
+            </h3>
+            {edition.body}
+            <Image
+              src={edition.imageSource}
+              width={600}
+              height={500}
+              alt={`wappulehti-${edition.year}`}
+              className='mx-auto'
+            />
+          </Fragment>
+        ))}
       </div>
     </div>
   )
