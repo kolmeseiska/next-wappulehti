0cb06eab58a62d723d47c30c94bfba364a0aae24
niemisami <sami@nieminen.fi>
2024-04-10 10:04:41 +0300

    Fix swap property broke fonts

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,6 +1,5 @@
 
-import { DM_Serif_Display } from 'next/font/google'
-import { Raleway } from 'next/font/google'
+import { DM_Serif_Display, Raleway } from 'next/font/google'
 
-export const dmSerifDisplay = DM_Serif_Display({ weight: '400', display: 'swap', subsets: ['latin'], variable: '--font-display' })
-export const raleway = Raleway({ display: 'swap', subsets: ['latin'], variable: '--font-body' })
+export const raleway = Raleway({ subsets: ['latin'], variable: '--font-body' })
+export const dmSerifDisplay = DM_Serif_Display({ weight: '400', subsets: ['latin'], variable: '--font-display' })
