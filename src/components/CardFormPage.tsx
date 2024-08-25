bde30639d5bb2c5f004cc5d2270f915d2583fc8e
niemisami <sami@nieminen.fi>
2024-08-25 16:42:07 +0300

    Align CardFormPage layout

--- src/components/CardFormPage.tsx
+++ src/components/CardFormPage.tsx
@@ -9,27 +9,23 @@ type Props = {
 
 const CardFormPage = ({ header, side, form, details }: Props) => {
   return (
-    <div className='py-16'>
+    <div className='py-16 max-w-5xl mx-auto'>
       <div className='prose prose-xl text-center mx-auto mb-10'>
         {header}
       </div>
       <div className='flex flex-col md:flex-row justify-center items-center mb-20'>
-        <div className='max-w-md'>
-          <article className='prose prose-xl prose-ponkeli pb-10 px-5 md:px-5 '>
-            {side}
-          </article>
-        </div>
-        <div className='card w-full max-w-md shadow-2xl bg-base-100 mx-5 md:mx-5'>
+        <article className='prose prose-xl prose-ponkeli pb-10 px-5 md:px-5 '>
+          {side}
+        </article>
+        <div className='card shadow-2xl w-full max-w-md bg-base-100 mx-5 md:mx-5'>
           <div className='card-body'>
             {form}
           </div>
         </div>
       </div>
-      <div>
-        <article className='prose prose-xl mx-auto pb-10 px-5 md:px-5'>
-          {details}
-        </article>
-      </div>
+      <article className='prose max-w-full prose-xl mx-auto pb-10 px-5 md:px-5'>
+        {details}
+      </article>
     </div>
   )
 }
