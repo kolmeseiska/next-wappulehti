fa288274e8b0b87b99ce8cc0310c909fde1803da
niemisami <sami@nieminen.fi>
2024-08-25 17:59:50 +0300

    Update no upcoming events text

--- src/components/NoUpcomingEvents.tsx
+++ src/components/NoUpcomingEvents.tsx
@@ -3,9 +3,12 @@ import React from 'react'
 const NoUpcomingEvents = () => {
   return (
     <div className='prose prose-lg mx-auto'>
-      <p>Pönkeli järjestää vuosittain hauskoja kaikille avoimia tapahtumia vuoden ympäri: Syksyllä järjestetään urheilutapahtuma Pönkeli Games vaihtuvalla teemalla, talvella saatat huomata erilaisia jekkuja ja kevät huipentuu tietenkin wapun aloittavaan tapahtumaan Pönkelijulkkareihin sekä Turun opiskelijaskenen parhaimpiin reiveihin, Pönkelireiveihin. </p>
-      <p>Ota Pönkelin somet seurantaan sivun alalaidasta, niin saat tuoreimmat tiedot tapahtumista ja uutisista! </p>
-      <p>Muutama kuva tähän jotenki tollee tai sitte niin et kaks noi pienempää on esim. oikeessa tai vasemmassa reunassa tekstin vieressä ja alimmainen alhaalla</p>
+      <p>
+        Pönkeli järjestää ympäri vuoden hauskoja kaikille avoimia tapahtumia: Syksyllä urheilutapahtuma Pönkeli Games vaihtuvalla teemalla, talvella saatat huomata erilaisia jekkuja ja kevät huipentuu tietenkin Wapun aloittavaan tapahtumaan, Pönkelijulkkareihin, sekä Turun opiskelijaskenen parhaimpiin reiveihin, Pönkelireiveihin.
+      </p>
+      <p>
+        Ota Pönkelin somet seurantaan sivun alalaidasta, niin saat tuoreimmat tiedot tapahtumista ja uutisista!
+      </p>
     </div>
     // Old fallback
     // <p className='text-center text-gray-500 mb-4'>
