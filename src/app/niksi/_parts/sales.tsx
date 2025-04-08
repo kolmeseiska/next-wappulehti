17a4bbcd27cfbaa5147ac1e950398c7fc0c253c8
niemisami <sami@nieminen.fi>
2025-04-08 23:59:08 +0300

    Initial sales dates

--- src/app/niksi/_parts/sales.tsx
+++ src/app/niksi/_parts/sales.tsx
@@ -1,27 +1,27 @@
 const saleTimes: { day: string, date: string, time: string, place: string }[] = [
-  // { day: 'ke', date: '2025-04-09', time: '14-19', place: 'Osakuntasali' },
-  // { day: 'to', date: '2025-04-10', time: '14-19', place: 'Osakuntasali' },
-  // { day: 'pe', date: '2025-04-11', time: '14-19', place: 'Osakuntasali' },
-  // { day: 'la', date: '2025-04-12', time: '12-19', place: 'Osakuntasali' },
-  // { day: 'su', date: '2025-04-13', time: '11-15', place: 'Osakuntasali' },
-  // { day: 'ma', date: '2025-04-14', time: '12-16', place: 'Osakuntasali' },
-  // { day: 'ti', date: '2025-04-15', time: '15-19', place: 'Osakuntasali' },
-  // { day: 'ke', date: '2025-04-16', time: '12-19', place: 'Osakuntasali' },
-  // { day: 'to', date: '2025-04-17', time: '12-19', place: 'Osakuntasali' },
-  // { day: 'pe', date: '2025-04-18', time: '15-19', place: 'Osakuntasali' },
-  // { day: 'la', date: '2025-04-19', time: '15-19', place: 'Osakuntasali' },
-  // { day: 'su', date: '2025-04-20', time: '12-16', place: 'Osakuntasali' },
-  // { day: 'ma', date: '2025-04-21', time: '12-16', place: 'Osakuntasali' },
-  // { day: 'ti', date: '2025-04-22', time: '15-19', place: 'Osakuntasali' },
-  // { day: 'ke', date: '2025-04-23', time: '15-19', place: 'Osakuntasali' },
-  // { day: 'to', date: '2025-04-24', time: '15-19', place: 'Osakuntasali' },
-  // { day: 'pe', date: '2025-04-25', time: '15-19', place: 'Osakuntasali' },
-  // { day: 'la', date: '2025-04-26', time: '15-19', place: 'Osakuntasali' },
-  // { day: 'su', date: '2025-04-27', time: '12-16', place: 'Osakuntasali' },
-  // { day: 'ma', date: '2025-04-28', time: '12-16', place: 'Osakuntasali' },
-  // { day: 'ti', date: '2025-04-29', time: '15-19', place: 'Osakuntasali' },
-  // { day: 'ke', date: '2025-04-30', time: '12-16', place: 'Tuomiokirkkotori' },
-  // { day: 'to', date: '2025-05-01', time: '14-18', place: 'Wappupicnic' },
+  { day: 'ke', date: '2025-04-09', time: '12-16', place: 'Osakuntasali' },
+  { day: 'to', date: '2025-04-10', time: 'Tulossa', place: 'Tulossa' },
+  { day: 'pe', date: '2025-04-11', time: '-', place: '-' },
+  { day: 'la', date: '2025-04-12', time: '-', place: '-' },
+  { day: 'su', date: '2025-04-13', time: '-', place: '-' },
+  { day: 'ma', date: '2025-04-14', time: '-', place: '-' },
+  { day: 'ti', date: '2025-04-15', time: '-', place: '-' },
+  { day: 'ke', date: '2025-04-16', time: '-', place: '-' },
+  { day: 'to', date: '2025-04-17', time: '-', place: '-' },
+  { day: 'pe', date: '2025-04-18', time: '-', place: '-' },
+  { day: 'la', date: '2025-04-19', time: '-', place: '-' },
+  { day: 'su', date: '2025-04-20', time: '-', place: '-' },
+  { day: 'ma', date: '2025-04-21', time: '-', place: '-' },
+  { day: 'ti', date: '2025-04-22', time: '-', place: '-' },
+  { day: 'ke', date: '2025-04-23', time: '-', place: '-' },
+  { day: 'to', date: '2025-04-24', time: '-', place: '-' },
+  { day: 'pe', date: '2025-04-25', time: '-', place: '-' },
+  { day: 'la', date: '2025-04-26', time: '-', place: '-' },
+  { day: 'su', date: '2025-04-27', time: '-', place: '-' },
+  { day: 'ma', date: '2025-04-28', time: '-', place: '-' },
+  { day: 'ti', date: '2025-04-29', time: '-', place: '-' },
+  { day: 'ke', date: '2025-04-30', time: '-', place: 'Tuomiokirkkotori' },
+  { day: 'to', date: '2025-05-01', time: '-', place: 'Wappupicnic' },
 ]
 
 const getCurrentDate = () => {
@@ -65,14 +65,9 @@ const Sales = () => {
           })}
         </tbody>
       </table>
-      {saleTimes.length
-        ? null
-        : (
-          <p className='text-amber-900 py-4'>
-            Hiomme vielä myyntiaikoja ja -paikkoja, joten seuraa sivua sekä someamme!
-          </p>
-        )
-      }
+      <p className='text-amber-900 py-4'>
+        Hiomme vielä myyntiaikoja ja -paikkoja, joten seuraa sivua sekä someamme!
+      </p>
     </div>
   )
 }
