debe13b6b53c6d18b9ce288b942bc2845966e7d6
niemisami <sami@nieminen.fi>
2025-08-18 13:48:54 +0300

    Bold

--- src/app/(main)/rekry/page.tsx
+++ src/app/(main)/rekry/page.tsx
@@ -13,7 +13,7 @@ const page = () => {
           Pönkeli rekrytoi uusia pönkeleitä tuottamaan Turun hauskinta wappulehteä aina myöhään keväällä ja syksyllä koulun alkaessa. Pönkelissä pääset tekemään kaikkea hauskaa - olitpa sitten piirtäjä, graafikko, vitsiniekka, tapahtuman tuottaja, valokuvaaja tai muuten vain valmis hassuttelemaan ja hyppäämään projektiin! Pönkeliin hakiessa et tarvitse mitään erityisiä taitoja, vain avointa mieltä.
         </p>
         <p>
-          Pönkeliin voivat hakea kaikki, olit sitten teekkari tai et, kunhan omaat hassuttelevan mielen ja halun tehdä yhdessä hyvää muille. Hae mukaan Pönkeliin alla olevalla lomakkeella, ja sinuun ollaan yhteydessä hakujen aikaan. Voit siit ilmoittaa kiinnostuksesi mihin aikaan vuodesta vain!
+          Pönkeliin voivat hakea kaikki, olit sitten teekkari tai et, kunhan omaat hassuttelevan mielen ja halun tehdä yhdessä hyvää muille. Hae mukaan Pönkeliin alla olevalla lomakkeella, ja sinuun ollaan yhteydessä hakujen aikaan. Voit siis ilmoittaa kiinnostuksesi mihin aikaan vuodesta vain!
 
         </p>
       </div>
