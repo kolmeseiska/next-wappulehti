bf916825f9653dd6d2726bfa62fbef8720831885
juoksuorava <lauriaslak@gmail.com>
2026-05-07 09:27:39 +0300

    Fixed typos and updated texts a bit

--- src/app/(main)/rekry/page.tsx
+++ src/app/(main)/rekry/page.tsx
@@ -10,17 +10,18 @@ const page = () => {
       </h1>
       <div className='prose mx-auto'>
         <p>
-          Pönkeli rekrytoi uusia toimittajia tuottamaan Turun hauskinta wappulehteä aina kesäisin ja syksyn alussa. Pönkelissä pääset tekemään kaikkea hauskaa - olitpa sitten kiinnostunet piirtämisestä, taittamisesta, kirjoittamisesta, vitsailusta, tapahtumatuottamisesta, valo- tai videokuvaamisesta, markkinoinnista tai muuten vain valmis hassuttelemaan ja hyppäämään projektiin! Pönkeliin hakiessa et tarvitse mitään erityisiä taitoja, vain avointa mieltä ja innon oppia uutta.
+          Pönkeli rekrytoi uusia toimittajia tuottamaan Turun hauskinta wappulehteä jälleen kerran. Pönkelissä pääset tekemään kaikkea hauskaa - olitpa sitten kiinnostunut piirtämisestä, taittamisesta, kirjoittamisesta, vitsailusta, tapahtumatuottamisesta, valo- tai videokuvaamisesta, markkinoinnista tai muuten vain valmis hassuttelemaan ja hyppäämään projektiin! Pönkeliin hakiessa et tarvitse mitään erityisiä taitoja, vain avointa mieltä ja innon oppia uutta.
         </p>
         <p>
-          Pönkeliin voivat hakea kaikki, olit sitten teekkari tai et, kunhan omaat hassuttelevan mielen ja halun tehdä yhdessä hyvää muille. Hae mukaan Pönkeliin alla olevalla lomakkeella, ja sinuun ollaan yhteydessä hakujen aikaan. Voit siis ilmoittaa kiinnostuksesi mihin aikaan vuodesta vain!
+          Pönkeliin voivat hakea kaikki, olit sitten teekkari tai et, kunhan omaat hassuttelevan mielen ja halun tehdä yhdessä hyvää muille. Hae mukaan Pönkeliin alla olevalla lomakkeella, ja sinuun voidaan olla yhteydessä hakuajan päättymisen jälkeen. Ilmoita kiinnostuksesi siis heti!
         </p>
-        <p>Erityisesti toimitukseen haetaan tällä hetkellä:</p>
+        <p>Erityisesti toimitukseen haetaan tällä hetkellä toimittajia jotka haluavat tehdä:</p>
         <ul>
-          <li>Piirtäjiä ja muita graafikoita</li>
-          <li>Tapahtumatuottajia</li>
-          <li>Valo- ja videokuvaajia</li>
-          <li>Markkinoijia ja sosiaalisen median sisällöntuottajia</li>
+          <li>Piirustuksia tai muuta grafiikkaa</li>
+          <li>Tapahtumia</li>
+          <li>Valo- ja/tai videokuvausta</li>
+          <li>Videoeditointia</li>
+          <li>Markkinointia ja sosiaalisen median sisältöä</li>
         </ul>
         <p>
           Rekry sulkeutuu virallisesti 31.5.2026
